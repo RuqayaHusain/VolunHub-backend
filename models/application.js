35f11041501650eb7a1a15ef4956417ba11b3665
Ruqaya Husain <ruqayanader22@gmail.com>
2025-11-10 10:26:52 +0300

    added attributes to the application model

--- models/application.js
+++ models/application.js
@@ -1,8 +1,28 @@
 const mongoose = require('mongoose');
 
 const applicationSchema = mongoose.Schema({
-
-});
+    status: {
+        type: String,
+        enum: ['pending', 'approved', 'rejected', 'completed'],
+        required: true,
+    },
+    hours: {
+        type: Number,
+        default: 0,
+    },
+    message: {
+        type: String,
+    },
+    event: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event',
+    },
+    volunteer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+},
+    { timestamps: true });
 
 const Application = mongoose.model('Application', applicationSchema);
 
