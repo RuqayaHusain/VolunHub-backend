e71c52875506468f76a9151daa20851f4e48c755
Ruqaya Husain <ruqayanader22@gmail.com>
2025-11-11 18:41:55 +0300

    added index route, that fetches all events

--- controllers/events.js
+++ controllers/events.js
@@ -6,7 +6,7 @@ const Event = require('../models/event.js');
 router.post('/', async (req, res) => {
     try {
         if (req.user.role !== 'organization') {
-            return res.status(403).json({ err: `Only organizations are allowed to create events ${req.user.role}`});
+            return res.status(403).json({ err: `Only organizations are allowed to create events ${req.user.role}` });
         }
 
         const createdEvent = await Event.create({
@@ -14,6 +14,15 @@ router.post('/', async (req, res) => {
             owner: req.user._id,
         });
         res.status(201).json(createdEvent);
+    } catch (err) {
+        res.status(500).json({ err: 'Something went wrong!' });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const events = await Event.find({}).populate('owner').sort({ createdAt: 'desc' });
+        res.status(200).json(events);
     } catch (err) {
         res.status(500).json({ err: 'Something went wrong!' });
     }
