34a8cd748d63b4e12719bd8ff4cd684d56d8904b
alialgallaf <alialgallaf06@gmail.com>
2025-11-13 14:38:54 +0300

    added deleteEvent functionality

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -63,8 +63,36 @@ const updateEvent= async (req, res) => {
 };
 
 
+const deleteEvent= async (req, res) => {
+  try {
+    const { eventId} = req.params;        
+    const organizationId = req.user._id;   
+
+
+    const event = await Event.findById(eventId);
+    if (!event) {
+      return res.status(404).json({ success: false, message: "Event not found" });
+    }
+
+    if (!event.organization.equals(organizationId)) {
+      return res.status(403).json({ success: false, message: "Youre not allowed to edit this event" });
+    }
+
+
+    await Event.findByIdAndDelete(eventId);
+    res.json({ success: true, message: "Event deleted successfully" });
+  } 
+  
+  catch (error) {
+    console.error(error);
+    
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
 module.exports = {
   getVolunteerApplications,
   getVolunteerTotalHours,
-  updateEvent
+  updateEvent,
+  deleteEvent
 };
