87eec7292928fdef02445d882ec290b3adadbc43
alialgallaf <alialgallaf06@gmail.com>
2025-11-13 14:39:22 +0300

    added delete event route to dashboard

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getVolunteerApplications, getVolunteerTotalHours, updateEvent } = require("../controllers/dashboardController");
+const { getVolunteerApplications, getVolunteerTotalHours, updateEvent, deleteEvent } = require("../controllers/dashboardController");
 const verifyToken = require("../middleware/verify-token");
 
 const router = express.Router();
@@ -7,5 +7,5 @@ const router = express.Router();
 router.get("/volunteer", verifyToken, getVolunteerApplications);
 router.get("/volunteer/total-hours", verifyToken, getVolunteerTotalHours);
 router.put("/event/:eventId", verifyToken, updateEvent);
-
+router.delete("/event/:eventId", verifyToken, deleteEvent);
 module.exports = router;
