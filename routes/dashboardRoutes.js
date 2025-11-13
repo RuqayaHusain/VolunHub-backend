3028b7cf79e9374dc8bd347b49c8417b7bb34244
alialgallaf <alialgallaf06@gmail.com>
2025-11-13 15:55:54 +0300

    added viewEventApplicants route to dashboard

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { getVolunteerApplications, getVolunteerTotalHours, updateEvent, deleteEvent } = require("../controllers/dashboardController");
+const { getVolunteerApplications, getVolunteerTotalHours, updateEvent, deleteEvent,viewEventApplicants  } = require("../controllers/dashboardController");
 const verifyToken = require("../middleware/verify-token");
 
 const router = express.Router();
 
 router.get("/volunteer", verifyToken, getVolunteerApplications);
 router.get("/volunteer/total-hours", verifyToken, getVolunteerTotalHours);
+router.get("/event/:eventId/applicants", verifyToken, viewEventApplicants);
 router.put("/event/:eventId", verifyToken, updateEvent);
 router.delete("/event/:eventId", verifyToken, deleteEvent);
+
 module.exports = router;
