bc0da1307ae41b30501dc98130f577a60f94568b
Manar's <manarmohd15@gmail.com>
2025-11-13 13:13:38 +0300

    feedback route

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -5,4 +5,7 @@ const verifyToken = require('../middleware/verify-token');
 
 router.post('/', verifyToken, feedbackController.createFeedback);
 router.get('/organization/:organizationId', verifyToken, feedbackController.getFeedbackByOrganization);
+router.get('/stats/:organizationId', verifyToken, feedbackController.getFeedbackStats);
+router.get('/voluteer', verifyToken, feedbackController.getFeedbackByVolunteer);
+
 module.exports = router;
