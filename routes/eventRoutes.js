fcc59395ba04c0add4925703af7d1b6dedf966eb
alialgallaf <alialgallaf06@gmail.com>
2025-11-16 09:40:08 +0300

    add update and delete routes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const verifyToken = require("../middleware/verify-token");
 
-const { createEvent, getEventList, showEvent ,deleteEvent, updateEvent, ApplyForEvents } = require('../controllers/eventController');
+const { createEvent, getEventList, showEvent ,deleteEvent, updateEvent } = require('../controllers/eventController');
 
 router.post('/', createEvent);
 router.get('/', getEventList);
 router.get('/:eventId', showEvent);
-router.get('/:eventId', showEvent);
-router.post('/:eventId/apply', ApplyForEvents);
-router.delete("/event/:eventId", verifyToken, deleteEvent);
+router.put('/:eventId', verifyToken, updateEvent);
+router.delete("/:eventId", verifyToken, deleteEvent);
 module.exports = router;
