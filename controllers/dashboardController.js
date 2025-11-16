476e74a3b36a2637163a1f00d4706d3f716a5e87
alialgallaf <alialgallaf06@gmail.com>
2025-11-16 09:36:58 +0300

    Add getOrganizationApplications to fetch all applications

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,12 +1,14 @@
+
+
 const Application = require("../models/application");
 const Event = require("../models/event");
 
 const getVolunteerApplications = async (req, res) => {
   try {
-    const volunteerId = req.user.id;
+    const volunteerId = req.user._id;
     
     const applications = await Application.find({ volunteer: volunteerId })
-      .populate("event", "title date location hours")
+.populate("event", "title date location duration")
 
     res.json({
       success: true,
@@ -26,7 +28,7 @@ const getVolunteerTotalHours = async (req, res) => {
     const totalHours = applications.reduce((sum, app) => sum + (app.hours || 0), 0);
     res.json({ success: true, totalHours });
   } catch (error) {
-    res.status(500).json({ success: false, message: 'Server error', error });
+    res.status(500).json({ success: false, message: 'Serve  r error', error });
   }
 };
 
@@ -38,6 +40,7 @@ const viewEventApplicants = async (req, res) => {
 
     const event = await Event.findById(eventId);
     if (!event) return res.status(404).json({ success: false, message: "Event not found" });
+    
     if (!event.owner.equals(organizationId)) {
       return res.status(403).json({ success: false, message: "You’re not allowed to see applicants for this event" });
     }
@@ -59,8 +62,34 @@ const viewEventApplicants = async (req, res) => {
 
 
 
+const getOrganizationApplications = async (req, res) => {
+  try {
+    const organizationId = req.user._id;
+
+    const events = await Event.find({ owner: organizationId }).select("_id title");
+
+    const eventIds = events.map(e => e._id);
+
+    const applications = await Application.find({ event: { $in: eventIds } })
+      .populate('event', 'title date location duration') 
+      .populate('volunteer', 'name email totalHours'); 
+
+    res.json({
+      success: true,
+      applications,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+
+
 module.exports = {
   getVolunteerApplications,
   getVolunteerTotalHours,
-  viewEventApplicants
+  viewEventApplicants,
+  getOrganizationApplications  
 };
