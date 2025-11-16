f8f9f2ba0b46451f3b59dc8877a0503382d3931d
alialgallaf <alialgallaf06@gmail.com>
2025-11-16 18:52:52 +0300

    ...

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
  const router = express.Router();
- const { ApplyForEvents } = require('../controllers/applicationController');
+ const {  updateApplicationStatus } = require('../controllers/applicationController');
+  const { ApplyForEvents  } = require('../controllers/eventController');
+
+const verifyToken = require("../middleware/verify-token");
 
  router.post('/', ApplyForEvents);
 
  router.put("/:applicationId/status", verifyToken, updateApplicationStatus);
 
- 
 module.exports = router;
